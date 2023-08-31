f1dcbc5c01a90f13c1a162fcc1bed512c1067aae
F110-008\bachelor <thomas.vappereau@saintmichelannecy.fr>
2023-08-31 10:47:07 +0200

    images

--- Pokétruc.js
+++ Pokétruc.js
@@ -1,75 +1,87 @@
-let pokearray = document.getElementById("pokearray")
-let poketableau = []
-let types = []
+let poketableau = [];
+let weaknesses = [];
 
 async function run() {
-    await getPokemons()
-    renderPokemon(poketableau)
+    await getPokemons();
+	await getWeakness();
+	renderPokemon(poketableau,weaknesses);
 }
 
 run()
 
 async function getPokemons() {
-    let result = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1008");
+    let result = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1010");
     let promises = [];
     result.data.results.map(function (pokemon) {
         promises.unshift(axios.get("https://pokeapi.co/api/v2/pokemon/" + pokemon.name));
 
     });
     poketableau = await Promise.all(promises);
+	console.log(poketableau);
+}
+
+async function getWeakness(){
+	let result = await axios.get("https://pokeapi.co/api/v2/type?limit=18");
+	let promises = [];
+	result.data.results.map(function (type) {
+		promises.unshift(axios.get("https://pokeapi.co/api/v2/type/" + type.name));
+	});
+
+	weaknesses = await Promise.all(promises);
+	console.log(weaknesses);
 }
 
 function renderPokemon(poketableau) {
     poketableau.map(function (result) {
-		var NAME = result.data.name;
-		var ID = result.data.id;
+		let NAME = result.data.name;
+		let ID = result.data.id;
+		let IMG = result.data.sprites.other["official-artwork"].front_default;
 		
-		if (ID == 108) {
-			var IMG = './antoine.png';
-		}
-		
-		var IMG = result.data.sprites.other["official-artwork"].front_default;
-		
-		var TYPE = getTypes(result.data.types);
-		
-		var HP = result.data.stats[0].base_stat;
-		var ATK = result.data.stats[1].base_stat;
-		var DEF = result.data.stats[2].base_stat;
-		var SPEATK = result.data.stats[3].base_stat;
-		var SPEDEF = result.data.stats[4].base_stat;
-		var SPD = result.data.stats[5].base_stat;
+		let types = getTypes(result.data.types);
+
+		let TYPES = '';
+		types.map( function (types){
+			TYPES = TYPES + `<td><img class="type" src="./IMG/`+types+`.png"/></td>`;
+		});
+
+		let HP = result.data.stats[0].base_stat;
+		let ATK = result.data.stats[1].base_stat;
+		let DEF = result.data.stats[2].base_stat;
+		let SPEATK = result.data.stats[3].base_stat;
+		let SPEDEF = result.data.stats[4].base_stat;
+		let SPD = result.data.stats[5].base_stat;
 		
         let affichage =
             `<tr>
 				<td><img id="artwork" src=`+IMG+`></td>
 				<td id="name">`+NAME+`</td>
 				<td id="id">`+ID+`</td>
-				<td><table><tr>`+TYPE+`</tr></table></td>
+				<td><table id="type"><tr>`+TYPES+`</tr></table></td>
 				<td>
 					<table>
 						<tr>
-							<td>HP</td>
+							<td class="stat-label">HP</td>
 							<td>`+HP+`</td>
 						  
 						</tr>
 						<tr>
-							<td>Attack</td>
+							<td class="stat-label">Attack</td>
 							<td>`+ATK+`</td>
 						</tr>
 						<tr>
-							<td>Defense</td>
+							<td class="stat-label">Defense</td>
 							<td>`+DEF+`</td>
 						</tr>
 						<tr>
-							<td>Special Attack</td>
+							<td class="stat-label">SPE Attack</td>
 							<td>`+SPEATK+`</td>
 						</tr>
 						<tr>
-							<td>Special Defense</td>
+							<td class="stat-label">SPE Defense</td>
 							<td>`+SPEDEF+`</td>
 						</tr>
 						<tr>
-							<td>Speed</td>
+							<td class="stat-label">Speed</td>
 							<td>`+SPD+`</td>
 						</tr>
 					</table>
@@ -80,9 +92,9 @@ function renderPokemon(poketableau) {
 }
 
 function getTypes(types) {
-    let a = "";
-    types.map(function (type) {
-        a = a + `<td id="type">${type.type.name}</td>`
+    let TYPE = [];
+	types.map(function (types) {
+		TYPE.push(types.type.name);
     });
-    return a;
+    return TYPE;
 }
